bc73bf314860385fd2e3e4869ea8779b61de9698
Bobby B. <rallycoding.bobby@gmail.com>
2025-01-22 13:22:50 -0600

    updated PAT

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Jan 25
+            Learn React Jan 25 #1
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
