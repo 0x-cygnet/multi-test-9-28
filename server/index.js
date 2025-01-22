a50c2d33c7d640cac974586efc6043e6ef244be7
Bobby B. <rallycoding.bobby@gmail.com>
2025-01-22 14:44:32 -0600

    upgrade redis/server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const keys = require('./keys');
+const keys = require("./keys");
 
 // Express App Setup
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Postgres Client Setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pgClient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
@@ -18,19 +18,19 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort,
   ssl:
-    process.env.NODE_ENV !== 'production'
+    process.env.NODE_ENV !== "production"
       ? false
       : { rejectUnauthorized: false },
 });
 
-pgClient.on('connect', (client) => {
+pgClient.on("connect", (client) => {
   client
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
     .catch((err) => console.error(err));
 });
 
 // Redis Client Setup
-const redis = require('redis');
+const redis = require("redis");
 const redisClient = redis.createClient({
   url: `redis://${keys.redisHost}:${keys.redisPort}`,
   retry_strategy: () => 1000,
@@ -44,12 +44,12 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/', (req, res) => {
-  res.send('Hi');
+app.get("/", (req, res) => {
+  res.send("Hi");
 });
 
-app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
 
   res.send(values.rows);
 });
@@ -59,11 +59,11 @@ app.get("/values/current", async (req, res) => {
   res.send(values);
 });
 
-app.post('/values', async (req, res) => {
+app.post("/values", async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
+    return res.status(422).send("Index too high");
   }
 
   await redisClient.hSet("values", index, "Nothing yet!");
@@ -74,5 +74,5 @@ app.post('/values', async (req, res) => {
 });
 
 app.listen(5000, (err) => {
-  console.log('Listening');
+  console.log("Listening");
 });
