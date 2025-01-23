1d66f0eddfc12adbf2211aa767a76e9fe3993766
Bobby B. <rallycoding.bobby@gmail.com>
2025-01-23 11:19:09 -0600

    test sg

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React test
+            Learn React test #2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
